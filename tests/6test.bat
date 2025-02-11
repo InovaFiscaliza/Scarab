24d7996f43bb286f2ce68f949da77381ee951d46
Fabio Santos Lobao <fabioslobao@hotmail.com>
2025-02-11 13:39:53 -0300

    Create havy load test

---
+++ tests/6test.bat
@@ -0,0 +1,26 @@
+@echo off
+REM Clean sandbox folder
+if exist sandbox (
+    rmdir /s /q sandbox
+)
+
+REM Extract test_basic.zip using 7zip
+tar -xf test_6.tgz
+
+REM Check if extraction was successful
+if %errorlevel% neq 0 (
+    echo Failed to extract test_basic.zip
+    exit /b %errorlevel%
+)
+
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo.
+echo Change columns and multiple keys
+echo.
+echo Run the script using: uv run ..\src\scarab.py .\sandbox\config.json
+echo.
+echo Check if the output is as described in README.md.
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+exit /b 0
