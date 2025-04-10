c19122d6353843b28cdb5440e9ad6d7f9a841555
Fabio Santos Lobao <fabioslobao@hotmail.com>
2025-04-10 08:25:41 -0300

    include update of default_config and examples

--- install/update_scarab.bat
+++ install/update_scarab.bat
@@ -1,5 +1,7 @@
 @echo off
 copy /Y ..\src\*.py C:\ProgramData\Anatel\Scarab\src
+copy /Y ..\src\default_config.json C:\ProgramData\Anatel\Scarab\src\default_config.json
+copy /Y ..\data\examples\*.json C:\ProgramData\Anatel\scarab\config
 
 REM.
 REM Updated files in the deployment directory from the local repository
