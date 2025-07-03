b105d464a89fe982d1d43cddb78c7313cb56d878
lobao <fabioslobao@hotmail.com>
2025-07-03 21:46:15 +0200

    Update null test

--- tests/0clean_test.bat
+++ tests/0clean_test.bat
@@ -4,11 +4,19 @@ if exist sandbox (
     rmdir /s /q sandbox
 )
 
+
+REM Extract test_basic.zip using 7zip
+tar -xf test_0.tgz
+
+REM Check if extraction was successful
+if %errorlevel% neq 0 (
+    echo Failed to extract test_basic.zip
+    exit /b %errorlevel%
+)
+
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo.
 echo Sandbox Cleaned
-echo.
-echo Run the test_1.bat script to setup the sandbox for the first test.
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
