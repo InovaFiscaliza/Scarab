e230742b58a28ca25466dd51951854f996ec1b70
Fabio Santos Lobao <fabioslobao@hotmail.com>
2025-07-07 06:35:53 -0300

    include uv files in update script

--- install/update_scarab.bat
+++ install/update_scarab.bat
@@ -2,6 +2,8 @@
 copy /Y ..\src\*.py C:\ProgramData\Anatel\Scarab\src
 copy /Y ..\src\default_config.json C:\ProgramData\Anatel\Scarab\src\default_config.json
 copy /Y ..\data\examples\*.json C:\ProgramData\Anatel\scarab\config
+copy /Y ..\pyproject.toml C:\ProgramData\Anatel\scarab
+copy /Y ..\uv.lock C:\ProgramData\Anatel\scarab
 
 REM.
 REM Updated files in the deployment directory from the local repository
